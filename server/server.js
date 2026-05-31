faac5d281b2e8e696ec51adef811d192f4e1e9a1
Abderazek <abderazekbelhadjmbarek@gmail.com>
2026-05-31 16:32:14 +0100

    fix server

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Enable CORS
+// Enable CORS   
 app.use(
   cors({
     origin: "http://localhost:5173",
